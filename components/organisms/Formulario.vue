c1616ffb5d04ebd8caf9aa6c3ccba7c215ee75d5
Denis78-sys <denismarques736gmail.com>
2024-01-22 10:02:36 -0300

    .

--- components/organisms/Formulario.vue
+++ components/organisms/Formulario.vue
@@ -2,12 +2,12 @@
 <template>
   <div class="formulario lx:w-4/5 lg:w-80 md:w-full">
     <BemVindo />
-    <p class="text-white text-gray-400 lg:text-xs sm:text-xs lg:mb-4 md:mb-2">
+    <p class="text-white text-gray-400 lg:text-xs sm:text-xs lg:mb-4 md:mb-2 sm:mb-1">
       O DM System fornece os blocos e componentes necessários para criar um
       ambiente verdadeiramente profissional.
     </p>
     <button
-      class="flex gap-4 justify-center items-center text-white text-xs bg-gray-600 lg:p-2 md:p-1 w-full mb-12 mt-9 rounded-2xl"
+      class="flex gap-4 justify-center items-center text-white text-xs bg-gray-600 lg:p-2 md:p-2 sm:p-2 w-full mb-12 mt-9 rounded-2xl"
     >
       <img src="@/assets/imagens/google.svg" alt="Logo do Google" /> Faça login
       no Google
@@ -17,7 +17,7 @@
         <label class="text-white text-xs">Endereço de E-mail</label>
         <input
           type="email"
-          class="bg-inherit border border-1 border-gray-600 text-white text-sm p-2 w-full rounded-2xl"
+          class="bg-inherit border border-1 border-gray-600 text-white text-sm lg:p-2 md:p-2 sm:p-2 w-full rounded-2xl"
           placeholder="exemplo@gmail.com"
           required
         />
@@ -26,7 +26,7 @@
         <label class="text-white text-xs">Senha</label>
         <input
           type="password"
-          class="bg-inherit border border-1 border-gray-600 text-white text-xs p-2 w-full rounded-2xl"
+          class="bg-inherit border border-1 border-gray-600 text-white text-xs lg:p-2 md:p-2 sm:p-2 w-full rounded-2xl"
           placeholder="*********"
           required
         />
@@ -37,7 +37,7 @@
       </div>
       <div>
         <button
-          class="flex gap-4 justify-center items-center text-white text-xs bg-blue-600 p-2 w-24 rounded-2xl"
+          class="flex gap-4 justify-center items-center text-white text-xs bg-blue-600 lg:p-2 md:p-1 sm:p-2 w-24 rounded-2xl"
         >
           Login
         </button>
